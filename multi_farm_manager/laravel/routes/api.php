1f0bbfc2c2bc41046f7513cd8d53d850f50c9c27
ZihanHossain <zihanhossain02@gmail.com>
2021-08-19 16:10:23 +0600

    Login Done

--- multi_farm_manager/laravel/routes/api.php
+++ multi_farm_manager/laravel/routes/api.php
@@ -31,7 +31,8 @@ Route::post('manager/cow/add', 'App\Http\Controllers\Cow_DetailsController@add_c
 
 Route::get('manager/newborn', 'App\Http\Controllers\New_Born_CowController@index');
 Route::get('manager/newborn/confirm/{id}', 'App\Http\Controllers\New_Born_CowController@confirm_cow');
-Route::post('login', 'App\Http\Controllers\LoginController@socialLogin');
+Route::post('login/google', 'App\Http\Controllers\LoginController@socialLogin');
+Route::post('login', 'App\Http\Controllers\LoginController@verify');
 Route::get('manager/milkcollection', 'App\Http\Controllers\Milk_CollectionController@index');
 Route::get('manager/milkcollection/cowdetails/{id}', 'App\Http\Controllers\Milk_CollectionController@details');
 Route::get('manager/profilesettings', 'App\Http\Controllers\Admin_ProfileController@index');
@@ -49,14 +50,14 @@ Route::get('manager/vaccination/check/{id}', 'App\Http\Controllers\Vaccinated_Co
 
 //worker
 
-Route::get('/cropworker/time/schedule','App\Http\Controllers\Time_ScheduleController@schedule');
-Route::get('/cropworker/time/schedule/{s_id}','App\Http\Controllers\Time_ScheduleController@process');
+Route::get('/cropworker/time/schedule', 'App\Http\Controllers\Time_ScheduleController@schedule');
+Route::get('/cropworker/time/schedule/{s_id}', 'App\Http\Controllers\Time_ScheduleController@process');
 
 //customer
 Route::get('/customer/profile', 'App\Http\Controllers\CustomerProfileController@profile');
 Route::get('/customer/dairies', 'App\Http\Controllers\DairiesController@index');
 Route::get('/customer/crops', 'App\Http\Controllers\CorpsController@index');
-Route::post('/sign_up/form','App\Http\Controllers\CustomerRegistrationController@registration');
+Route::post('/sign_up/form', 'App\Http\Controllers\CustomerRegistrationController@registration');
 Route::get('/customer/dairies/details/{id}', 'App\Http\Controllers\DairiesController@details');
 Route::post('/customer/dairies/details/{id}', 'App\Http\Controllers\CartController@index');
 Route::post('/customer/crops/details/{id}', 'App\Http\Controllers\CartController@index');
--- multi_farm_manager/react/src/components/Login.js
+++ multi_farm_manager/react/src/components/Login.js
@@ -1,23 +1,107 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom';
 import { googleProvider } from "../config/authMethord";
 import socialMediaAuth from '../service/auth';
+import { useCookies } from 'react-cookie';
+import { Button, Form, Alert } from 'react-bootstrap';
 
 function Login() {
+
+    // const [user, setUser] = useState([]);
+    const [username, setUserName] = useState([]);
+    const [password, setPassword] = useState([]);
+    const [message, setMessage] = useState('');
+    const [cookies, setCookie] = useCookies(['type']);
+
+    const history = useHistory();
+
+    const handleUserNameChange = (e) => {
+        setUserName(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
 
     const handleGoogleLogin = async(provider) => {
         const res = await socialMediaAuth(provider);
         console.log(res.displayName);
         console.log(res.email);
-        const url = `http://127.0.0.1:8000/api/login`;
+        const data = {
+            'name': res.displayName, 'email': res.email
+        }
+        console.log(data);
+        const url = `http://127.0.0.1:8000/api/login/google`;
         const axios = require('axios').default;
         axios({
             method: 'post',
             url: url,
-            data:[res.displayName, res.email],
+            data:data,
         }).then(response => {
+            setCookie('name', response.data.name, { path: '/' });
+            setCookie('email', response.data.email, { path: '/' });
+            setCookie('u_id', response.data.u_id, { path: '/' });
+            setCookie('type', response.data.type, { path: '/' });
+            console.log(cookies.email);
+            console.log(cookies.name);
+            console.log(cookies.u_id);
+            console.log(cookies.type);
             console.log(response.data);
+            if(response.data.type == 'manager')
+            {
+            history.push('/manager/home');
+            }
         })
+    }
+
+    const handleLoginIn = async () =>
+    {
+        if(typeof(username) && typeof(password) == "string")
+        {
+            const data = {
+                username, password
+            }
+            const url = `http://127.0.0.1:8000/api/login`;
+            const axios = require('axios').default;
+            await axios({
+                method: 'post',
+                url: url,
+                data:data,
+            }).then( response => {
+                if(response.data == 'error')
+                {
+                    console.log('Username or password is wrong');
+                }
+                else{
+                    setCookie('name', response.data.name, { path: '/' });
+                    setCookie('email', response.data.email, { path: '/' });
+                    setCookie('u_id', response.data.u_id, { path: '/' });
+                    setCookie('type', response.data.type, { path: '/' });
+                    console.log(cookies.email);
+                    console.log(cookies.name);
+                    console.log(cookies.u_id);
+                    console.log(cookies.type);
+                    console.log(response.data);
+                    if(response.data.type == 'manager')
+                    {
+                    history.push('/manager');
+                    }
+                    else if(response.data.type == 'customer')
+                    {
+                    history.push('/customer');
+                    }
+                    if(response.data.type == 'crop')
+                    {
+                    history.push('/cropworker');
+                    }
+                }
+            })
+        }
+        else
+        {
+            setMessage('Please enter User Name and Password');
+        }
     }
 
     return (
@@ -29,30 +113,39 @@ function Login() {
                 <h2>Sign Up Here</h2>
                 <span>Log in to your account using username and password</span>
                 <br/><br/>
-                <form>
-                    <table border='2'>
+                <Form>
+                    <table border='2' width='20%'>
                         <tr>
-                            <td><input type='text' name='uname' value='' placeholder='Username'></input></td>
+                            <td>
+                                <Form.Group className="mb-3" >
+                                    <Form.Label>User Name</Form.Label>
+                                    <Form.Control type="text" onChange={handleUserNameChange} placeholder="Enter name" />
+                                </Form.Group>
+                            </td>
                         </tr>
                         <tr>
-                            <td><input type='text' name='password' value='' placeholder='Password'></input></td>
+                            <td>
+                                <Form.Group className="mb-3" >
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" onChange={handlePasswordChange} placeholder="Password" />
+                                </Form.Group>
+                            </td>
                         </tr>
+                        <Alert variant='warning'>
+                            {message}
+                        </Alert>
                         <br />
                         <tr>
-                            <center><td><input type='submit' name='login' value='Login'></input></td></center>
+                            <center><Button onClick={handleLoginIn}>Log In</Button></center>
                         </tr>
                     </table>
                     <center><span>New Member,Please <Link to="/signup">Sign Up</Link></span></center>
-                </form>
+                </Form>
             </center>
             <button onClick={() => handleGoogleLogin(googleProvider)}>Google</button>
-            <Link to="/manager"><button>LOL</button></Link>
             <Link to="/customer"><button>Customer Dashboard</button></Link>
-
             <Link to="/cropworker"><button>CropWorker Dashboard</button></Link>
 
-            <button onClick={() => handleGoogleLogin(googleProvider)}>Google</button>
-            <Link to="/manager"><button>LOL</button></Link>
 
 
         </div>
